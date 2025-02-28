4478a0615a4a266cc7e492e4d6218aeed0080286
arifurrahmansajid <iamnobody3456@gmail.com>
2025-03-01 00:47:37 +0600

    first commit

---
+++ js/changepage.js
@@ -0,0 +1,8 @@
+// new-today
+document.getElementById("new-today").addEventListener("click", function () {
+    window.location.href = "../main.html";
+});
+// back-desk
+document.getElementById("back-desk").addEventListener("click", function () {
+    window.location.href = "../index.html"; 
+});
